e71f2c2ed48817cf6f70798f785d7192438272ea
haveyaseen <haveyaread@gmail.com>
2021-03-03 11:39:33 +0100

    make main element fullscreen if on chat page

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,42 +1,62 @@
 <script lang="ts">
-	import { scale } from "svelte/transition";
-	import MainNav from '../comp/layout/MainNav.svelte';
-	import Footer from '../comp/layout/Footer.svelte';
+    import { scale } from "svelte/transition"
+    import MainNav from "../comp/layout/MainNav.svelte"
+    import Footer from "../comp/layout/Footer.svelte"
 
-	export let segment: string | undefined;
+    export let segment: string | undefined
+    const FULLSCREEN_SEGMENTS = ["chat"]
 
-  const timeout = 150;
-  let show = true;
+    const timeout = 150
+    let show = true
 
-  const change = () => {
-    show = !show;
-  };
+    const change = () => {
+        show = !show
+    }
 
-  $: segment, change();
+    $: segment, change()
+    $: fullscreen = segment ? FULLSCREEN_SEGMENTS.includes(segment) : false
 </script>
 
-<style>
-	main {
-		position: relative;
-		background-color: white;
-		box-sizing: border-box;
-	}
-</style>
-
-<MainNav {segment}/>
+{#if !fullscreen}
+    <MainNav {segment} />
+{/if}
 
 {#if show}
-  <main
-    in:scale={{ duration: timeout, delay: timeout }}
-    out:scale={{ duration: timeout }}>
-    <slot />
-  </main>
-  <Footer />
+    <main
+        in:scale={{ duration: timeout, delay: timeout }}
+        out:scale={{ duration: timeout }}
+        class:fullscreen
+    >
+        <slot />
+    </main>
+    {#if !fullscreen}
+        <Footer />
+    {/if}
 {:else}
-  <main
-    in:scale={{ duration: timeout, delay: timeout }}
-    out:scale={{ duration: timeout }}>
-    <slot />
-  </main>
-  <Footer />
+    <main
+        in:scale={{ duration: timeout, delay: timeout }}
+        out:scale={{ duration: timeout }}
+        class:fullscreen
+    >
+        <slot />
+    </main>
+    {#if !fullscreen}
+        <Footer />
+    {/if}
 {/if}
+
+<style>
+    main {
+        position: relative;
+        background-color: white;
+        box-sizing: border-box;
+    }
+
+    main.fullscreen {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
+</style>

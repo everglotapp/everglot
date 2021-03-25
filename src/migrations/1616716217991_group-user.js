cbe7c9ff5782a28377bf41ea25729b3df4e5130e
senyaoh <senyao.hou@gmail.com>
2021-03-26 00:53:29 +0100

    Added migration files for groups

---
+++ src/migrations/1616716217991_group-user.js
@@ -0,0 +1,37 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined
+
+exports.up = (pgm) => {
+    // pgm.createType('user_type', ['learner', 'native']);
+    pgm.createTable({ schema: "app_public", name: "group_user" }, {
+        id: "id",
+        user_id: {
+            type: "serial",
+            references: { schema: "app_public", name: "users" },
+            notNull: true,
+        },
+        group_id: {
+            type: "serial",
+            references: { schema: "app_public", name: "groups" },
+            notNull: true,
+        },
+        // user_type: {
+        //     type: "user_type",
+        //     notNull: true,
+        // },
+        joined_on: {
+            type: "timestamp",
+            notNull: true,
+            default: pgm.func("current_timestamp"),
+        },
+        last_active: {
+            type: "timestamp",
+            notNull: false,
+        }
+    })
+}
+
+exports.down = (pgm) => {
+    pgm.dropTable({ schema: "app_public", name: "group_user" })
+}

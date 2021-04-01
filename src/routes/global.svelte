53f87160ed0fe7e8a74173b652363da30f65088f
haveyaseen <haveyaread@gmail.com>
2021-04-01 16:23:39 +0200

    add German, English, Chinese buttons to Global page

--- src/routes/global.svelte
+++ src/routes/global.svelte
@@ -1,5 +1,27 @@
-<div
-    class="my-16 container max-w-sm text-xl font-light p-8 bg-primary-lightest rounded-xl"
->
-    This page will be available soon!
+<script lang="ts">
+    import ButtonLarge from "../comp/util/ButtonLarge.svelte"
+</script>
+
+<svelte:head>
+    <title>Global – Everglot</title>
+</svelte:head>
+
+<div class="container flex w-auto gap-x-4">
+    <div
+        class="my-16 max-w-sm text-xl font-light p-8 bg-gray-lightest rounded-xl text-center flex justify-center"
+    >
+        <ButtonLarge tag="button">German</ButtonLarge>
+    </div>
+
+    <div
+        class="my-16 max-w-sm text-xl font-light p-8 bg-gray-lightest rounded-xl text-center flex justify-center"
+    >
+        <ButtonLarge tag="button">English</ButtonLarge>
+    </div>
+
+    <div
+        class="my-16 max-w-sm text-xl font-light p-8 bg-gray-lightest rounded-xl text-center flex justify-center"
+    >
+        <ButtonLarge tag="button">Chinese</ButtonLarge>
+    </div>
 </div>

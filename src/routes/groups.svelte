95d44040c4e26de1a24b3a54f7c15b58b27b1463
haveyaseen <haveyaread@gmail.com>
2021-04-05 13:21:02 +0200

    show private groups on groups page, generate General groups for English, German and Chinese

--- src/routes/groups.svelte
+++ src/routes/groups.svelte
@@ -1,9 +1,34 @@
 <script lang="ts">
     import ButtonLarge from "../comp/util/ButtonLarge.svelte"
+
+    import { query } from "@urql/svelte"
+
+    import { allGroups } from "../stores"
+
+    query(allGroups)
+
+    $: groups =
+        allGroups && !$allGroups.fetching && !$allGroups.error
+            ? $allGroups.data?.groups?.nodes
+                  .filter((group) => group && group.global === false)
+                  .map((group) => group!)
+            : []
 </script>
 
-<div
-    class="my-16 container max-w-sm text-xl font-light p-8 bg-gray-lightest rounded-xl text-center flex justify-center"
->
-    <ButtonLarge href="/chat">Go to the chat</ButtonLarge>
+<div class="container flex w-auto gap-x-4">
+    <div
+        class="my-16 max-w-sm text-xl font-light p-8 bg-gray-lightest rounded-xl text-center justify-center shadow-sm"
+    >
+        <h3 class="text-xl text-gray-bitdark">Your groups</h3>
+        {#each groups as group (group.uuid)}
+            <div
+                class="max-w-sm text-xl font-light p-8 bg-gray-lightest rounded-xl text-center justify-center shadow-sm"
+            >
+                <ButtonLarge
+                    href={`/chat?lang=${group.language?.englishName}&group=${group.uuid}`}
+                    >{group.groupName}</ButtonLarge
+                >
+            </div>
+        {/each}
+    </div>
 </div>

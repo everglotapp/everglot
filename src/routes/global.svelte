427bfe86bb496432dfe0b1b89b996c1d8772b35f
haveyaseen <haveyaread@gmail.com>
2021-04-05 12:39:56 +0200

    add sub-routes for topic-based chatrooms

--- src/routes/global.svelte
+++ src/routes/global.svelte
@@ -1,54 +1,23 @@
 <script lang="ts">
     import ButtonLarge from "../comp/util/ButtonLarge.svelte"
-    import { query } from "@urql/svelte"
-
-    import { allGroups } from "../stores"
-    import type { Language, Group } from "../types/generated/graphql"
-
-    query(allGroups)
-
-    function groupUuid(lang: Language["alpha2"]): Group["uuid"] {
-        const group = $allGroups.data?.groups?.nodes
-            .filter(Boolean)
-            .find(
-                (group) =>
-                    group!.global === true && group!.language?.alpha2 === lang
-            )
-        if (!group) {
-            return ""
-        }
-        return group.uuid
-    }
 </script>
 
-{#if $allGroups.fetching}
-    …
-{:else if $allGroups.error}
-    error
-{:else}
-    <div class="container flex w-auto gap-x-4">
-        <div
-            class="my-16 max-w-sm text-xl font-light p-8 bg-gray-lightest rounded-xl text-center flex justify-center shadow-sm"
-        >
-            <ButtonLarge href={`/chat?lang=German&group=${groupUuid("de")}`}
-                >German</ButtonLarge
-            >
-        </div>
-
-        <div
-            class="my-16 max-w-sm text-xl font-light p-8 bg-gray-lightest rounded-xl text-center flex justify-center shadow-sm"
-        >
-            <ButtonLarge href={`/chat?lang=English&group=${groupUuid("en")}`}
-                >English</ButtonLarge
-            >
-        </div>
-
-        <div
-            class="my-16 max-w-sm text-xl font-light p-8 bg-gray-lightest rounded-xl text-center flex justify-center shadow-sm"
-        >
-            <ButtonLarge href={`/chat?lang=Chinese&group=${groupUuid("zh")}`}
-                >Chinese</ButtonLarge
-            >
-        </div>
+<div class="container flex w-auto gap-x-4">
+    <div
+        class="my-16 max-w-sm text-xl font-light p-8 bg-gray-lightest rounded-xl text-center flex justify-center shadow-sm"
+    >
+        <ButtonLarge href="/global/de">German</ButtonLarge>
     </div>
-{/if}
+
+    <div
+        class="my-16 max-w-sm text-xl font-light p-8 bg-gray-lightest rounded-xl text-center flex justify-center shadow-sm"
+    >
+        <ButtonLarge href="/global/en">English</ButtonLarge>
+    </div>
+
+    <div
+        class="my-16 max-w-sm text-xl font-light p-8 bg-gray-lightest rounded-xl text-center flex justify-center shadow-sm"
+    >
+        <ButtonLarge href="/global/zh">Chinese</ButtonLarge>
+    </div>
+</div>

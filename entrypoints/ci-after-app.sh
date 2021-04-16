07930f31c116fada1204083895217cc71a9a8765
haveyaseen <haveyaread@gmail.com>
2021-04-16 21:26:52 +0200

    give app more time to start in CI

--- entrypoints/ci-after-app.sh
+++ entrypoints/ci-after-app.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-wait4ports -t 10 "tcp://everglot-db:5432"
-wait4ports -t 10 "tcp://everglot-app:3000"
+wait4ports -t 3 "tcp://everglot-db:5432"
+wait4ports -t 20 "tcp://everglot-app:3000"
 
 exec "$@"

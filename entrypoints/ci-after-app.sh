a3d470b8fe3615b2cd4fdf368d479238901552b7
haveyaseen <haveyaread@gmail.com>
2021-04-16 21:19:28 +0200

    run migrations in ci-after-db.sh

--- entrypoints/ci-after-app.sh
+++ entrypoints/ci-after-app.sh
@@ -2,9 +2,6 @@
 set -e
 
 wait4ports -t 10 "tcp://everglot-db:5432"
-
-npm run migrate up
-
 wait4ports -t 10 "tcp://everglot-app:3000"
 
 exec "$@"
--- entrypoints/ci-after-db.sh
+++ entrypoints/ci-after-db.sh
@@ -3,4 +3,6 @@ set -e
 
 wait4ports -t 10 "tcp://everglot-db:5432"
 
+npm run migrate up
+
 exec "$@"

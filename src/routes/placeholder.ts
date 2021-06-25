27bc069e8331ce588319a76e0d252562c0fd05b4
haveyaseen <haveyaread@gmail.com>
2021-06-26 00:19:53 +0200

    remove empty placeholder route

--- src/routes/placeholder.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response } from "express"
-
-export function get(_req: Request, res: Response, _next: () => void) {
-    res.status(200).send(
-        "<!DOCTYPE html><html><head></head><body></body></html>"
-    )
-}

cd99f50ce86e0aef324355acdf0985731ce1d336
haveyaseen <haveyaread@gmail.com>
2021-07-04 17:47:55 +0200

    add Carbon split screen icon

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-const sveltePreprocess = require("svelte-preprocess");
+const sveltePreprocess = require("svelte-preprocess")
+
+const { optimizeCarbonImports } = require("carbon-components-svelte/preprocess")
 
 module.exports = {
-  preprocess: sveltePreprocess({ postcss: true }),
-};
+    preprocess: [sveltePreprocess({ postcss: true }), optimizeCarbonImports()],
+}

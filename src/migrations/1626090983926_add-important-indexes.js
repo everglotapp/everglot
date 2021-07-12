f365bb0bedeb742d820babc57b4f42fdedfe067d
haveyaseen <haveyaread@gmail.com>
2021-07-12 14:11:42 +0200

    EVG-232: Speed up message loading (#141)
    * add indexes to important columns, especially in group_users, speeding up message loading

---
+++ src/migrations/1626090983926_add-important-indexes.js
@@ -0,0 +1,27 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined
+
+exports.up = (pgm) => {
+    pgm.createIndex({ schema: "app_public", name: "group_users" }, "user_id")
+    pgm.createIndex({ schema: "app_public", name: "group_users" }, "group_id")
+    pgm.createIndex({ schema: "app_public", name: "groups" }, "language_id")
+    pgm.createIndex(
+        { schema: "app_public", name: "groups" },
+        "language_skill_level_id"
+    )
+    pgm.createIndex({ schema: "app_public", name: "invite_tokens" }, "user_id")
+    pgm.createIndex({ schema: "app_public", name: "users" }, "google_id")
+}
+
+exports.down = (pgm) => {
+    pgm.dropIndex({ schema: "app_public", name: "users" }, "google_id")
+    pgm.dropIndex({ schema: "app_public", name: "invite_tokens" }, "user_id")
+    pgm.dropIndex(
+        { schema: "app_public", name: "groups" },
+        "language_skill_level_id"
+    )
+    pgm.dropIndex({ schema: "app_public", name: "groups" }, "language_id")
+    pgm.dropIndex({ schema: "app_public", name: "group_users" }, "group_id")
+    pgm.dropIndex({ schema: "app_public", name: "group_users" }, "user_id")
+}

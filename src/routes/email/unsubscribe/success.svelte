121f45daa8106953f2f27e3eb3c6437195d1c5cd
haveyaseen <haveyaread@gmail.com>
2021-07-18 14:43:52 +0200

    add horizontal padding to mobile success page for email unsubscribe

--- src/routes/email/unsubscribe/success.svelte
+++ src/routes/email/unsubscribe/success.svelte
@@ -6,7 +6,7 @@
 
 <div class="container py-8 md:py-16 max-w-xl text-center">
     <Headline1><Localized id="email-unsubscribe-success-headline" /></Headline1>
-    <p class="text-xl font-bold font-secondary text-gray-bitdark">
+    <p class="text-xl font-bold font-secondary text-gray-bitdark px-2">
         <Localized id="email-unsubscribe-success-description" />
     </p>
 </div>

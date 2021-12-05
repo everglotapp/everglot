8396ea606ecbb0546342a198c82b6f6a46dbb916
haveyaseen <haveyaread@gmail.com>
2021-12-06 00:41:27 +0100

    add default values for SESSION_COOKIE_NAME

--- src/__tests__/unit/session.ts
+++ src/__tests__/unit/session.ts
@@ -1,10 +1,12 @@
 import { getSessionIdCookieValue } from "../utils"
 
+const { SESSION_COOKIE_NAME } = process.env
+
 describe("getSessionCookieValue", () => {
     it("works as expected", () => {
         const headers = {
             "set-cookie": [
-                "everglot_sid=s%3Am9MaPp1xZJNUxQPp1xZJNUxQ4GvrYLPVz32JoIm61_YNLwr_Pp1xZJNUxQ4GvrYLMaPp1xZJNUxQ4GvrYLDW%2BNieJNUxQ4Gvo; Path=/; Expires=Mon, 06 Dec 2021 10:08:19 GMT; HttpOnly; SameSite=Strict",
+                `${SESSION_COOKIE_NAME}=s%3Am9MaPp1xZJNUxQPp1xZJNUxQ4GvrYLPVz32JoIm61_YNLwr_Pp1xZJNUxQ4GvrYLMaPp1xZJNUxQ4GvrYLDW%2BNieJNUxQ4Gvo; Path=/; Expires=Mon, 06 Dec 2021 10:08:19 GMT; HttpOnly; SameSite=Strict`,
             ],
         }
         expect(getSessionIdCookieValue(headers)).toBe(
